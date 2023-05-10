c32ed6b0eae9eab46f6cf562905a6c5a2119498f
Luan-Vinicius <116471758+Luan-Vinicius@users.noreply.github.com>
2023-05-10 06:36:25 -0300

    update exercise portifolio

--- exercicios_marcoBruno/html-css-feliz/script.js
+++ exercicios_marcoBruno/html-css-feliz/script.js
@@ -1,3 +1,18 @@
-function acionar(){
+var arrowLeft = document.getElementsByClassName('arrow')[0]
+var arrowRight = document.getElementsByClassName('arrow')[1]
+var fundo = document.querySelector('.box-imgCards')
+let px = 15
+
+
+arrowLeft.addEventListener('click', function(){
+    fundo.style='background-color: #000'
+    fundo.style=`transform: translateX(${px}px)`
+    px=px+15
+})
+
+arrowRight.addEventListener('click', function(){
+    px=px-15
+    fundo.style='background-color: #fff'
+    fundo.style=`transform: translateX(${px}px)`
     
-}
+})
