922da264c2464a146e21f9cc53e19532dd786c3c
Luan-Vinicius <116471758+Luan-Vinicius@users.noreply.github.com>
2023-05-13 07:07:24 -0300

    update exercise portifolio
    section "Sobre" finalizado

--- exercicios_marcoBruno/html-css-feliz/script.js
+++ exercicios_marcoBruno/html-css-feliz/script.js
@@ -1,18 +1,17 @@
 var arrowLeft = document.getElementsByClassName('arrow')[0]
 var arrowRight = document.getElementsByClassName('arrow')[1]
-var fundo = document.querySelector('.box-imgCards')
-let px = 15
+var fundo = document.querySelector('.gallery')
+let px = 25
 
 
 arrowLeft.addEventListener('click', function(){
-    fundo.style='background-color: #000'
+    px=px-25
     fundo.style=`transform: translateX(${px}px)`
-    px=px+15
+    
 })
 
 arrowRight.addEventListener('click', function(){
-    px=px-15
-    fundo.style='background-color: #fff'
+    px=px+25
     fundo.style=`transform: translateX(${px}px)`
     
 })
