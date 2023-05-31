b6a788a7553d2ab8ff5683e81e004fae1d37bb35
Luan-Vinicius <116471758+Luan-Vinicius@users.noreply.github.com>
2023-05-31 19:22:33 -0300

    update_5 - pratica 9
    section sobre

--- pagina 2/practicing/9nine/JS/components/main-carousel.js
+++ pagina 2/practicing/9nine/JS/components/main-carousel.js
@@ -1,14 +1,14 @@
 const rightArrow = window.document.querySelector('#arrowRight');
 const leftArrow = window.document.querySelector('#arrowLeft');
 const boxGalleryThree  = window.document.querySelector('.box-gallery._three');
-let pixels = 50;
+let pixels = 25;
 
 rightArrow.addEventListener('click', function(){
-    pixels = pixels + 50;
+    pixels = pixels + 25;
     boxGalleryThree.style = `transform: translateX(${pixels}px);`;
 });
 
 leftArrow.addEventListener('click', function(){
-    pixels = pixels - 50;
+    pixels = pixels - 25;
     boxGalleryThree.style = `transform: translateX(${pixels}px);`;
 });
