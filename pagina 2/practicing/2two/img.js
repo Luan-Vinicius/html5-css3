381efd8e22e74eec88a7ab066779fd8a20c54669
Luan-Vinicius <116471758+Luan-Vinicius@users.noreply.github.com>
2023-06-27 06:23:05 -0300

    update

---
+++ pagina 2/practicing/2two/img.js
@@ -0,0 +1,6 @@
+function addimg(){
+    return`
+        img src="imgs/blue_img_350x250.jpg" alt="">
+    `
+}
+export default addimg
---
+++ pagina 2/practicing/2two/main.js
@@ -0,0 +1,6 @@
+import img from "./img"
+
+const $img = img()
+const $root = document.querySelector(".root")
+
+$root.insertAdjacentHTML($img)
