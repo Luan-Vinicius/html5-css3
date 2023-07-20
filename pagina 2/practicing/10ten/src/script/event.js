617392f20c73d0dc5efcfe3d7c1f1a5e72e74dd9
Luan-Vinicius <116471758+Luan-Vinicius@users.noreply.github.com>
2023-07-20 05:04:54 -0300

    update

---
+++ pagina 2/practicing/10ten/src/script/event.js
@@ -0,0 +1,22 @@
+
+
+let $ageopts = document.getElementsByClassName("age-opts")
+console.log($ageopts)
+let dest
+for(v of $ageopts){
+   if(v.getAttribute('class')=="age-opts _destaque"){
+    dest = v
+   }
+}
+$ageopts = [...$ageopts]
+$ageopts.map((el)=>{
+    el.addEventListener('click',()=>{
+        
+        if(el.getAttribute('class')=="age-opts _destaque"){
+        }else{
+            dest.setAttribute('class','age-opts')
+            el.setAttribute('class','age-opts _destaque')
+            dest = el
+        }
+    })
+})
