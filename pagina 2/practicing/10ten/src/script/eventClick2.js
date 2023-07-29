ff47694fa030585dd359988373f62e7a8c677d2f
Luan-Vinicius <116471758+Luan-Vinicius@users.noreply.github.com>
2023-07-29 05:40:42 -0300

    update

--- pagina 2/practicing/10ten/src/script/eventClick2.js
+++ pagina 2/practicing/10ten/src/script/eventClick2.js
@@ -1,9 +1,28 @@
 const $alink = [...document.querySelectorAll(".gallery-imgs > div > .main-wrap")]
-
 console.log($alink)
 
-$alink.map((elem)=>{
+// $alink.map((elem)=>{
+//     elem.addEventListener("click",()=>{
+//         $Childrens = [...elem.children]
+//     })
+// })
+
+
+
+
+
+ind_element = $alink.find((elem, ind)=>{
     elem.addEventListener("click",()=>{
-        const $Childrens = [...elem.children]
+        ind
     })
 })
+console.log(ind_element)
+
+const teste = $alink.find((elem, ind)=>{
+    elem.addEventListener("click",()=>{
+        ind == ind_element
+    })
+    return elem
+})
+
+console.log(teste)
